6e4ceeb639f2af057ea162b9fbeff06542c48e1a
Artur Stanaszek <arturstan@gmail.com>
2019-01-05 20:32:53 +0100

    Controller and views for authors. Actions index, new

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,33 @@
+class AuthorsController < ApplicationController
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to authors_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+    @authors = Author.all
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :surname)
+  end
+end
